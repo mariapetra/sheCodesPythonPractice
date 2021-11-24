802a6e5edbdf3fb84a884986e50529ee0986172e
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2021-11-24 21:11:16 +0800

    practiced lists and inputs

--- practice/input.py
+++ practice/input.py
@@ -2,7 +2,17 @@
 # age = input("What is your age?:")
 # print(f"Hello {name}, you are {age} years young")
 
-num1 = float(input('Enter a digit: '))
-num2 = float(input('Enter a second number:'))
-answer = num1 * num2
-print(answer)
+# num1 = float(input('Enter a digit: '))
+# num2 = float(input('Enter a second number:'))
+# answer = num1 * num2
+# print(answer)
+
+
+user = input("What is your name?: ")
+dist_km = float(input("How far did you run in kms?: "))
+km_to_miles = float((dist_km / 1.609))
+
+print(f"""Hey {user.capitalize()}, conrats on running {dist_km}kms. 
+Did you realise that is {round(km_to_miles,1)} in miles""")
+
+#Round will round the number
