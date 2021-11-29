7e9c6c832fb45e1724fd7fe8c46790d88a858c70
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2021-11-29 21:37:13 +0800

    Create exercises.py

---
+++ practice/exercises.py
@@ -0,0 +1,73 @@
+# For loops exercises
+
+user_input = int(input("Enter a number: "))
+
+for i in user_input:
+    print(f"{user_input} * {i} = {user_input}"*i)
+
+
+#Loops: Exercise (Extension)
+
+# 1 *************** come back to************************
+
+
+#Q2) Ask the user to enter a string. 
+# Output the string one character at a time, 
+# as well as (output) it’s(the characters) position in the string. 
+
+#Break down question:
+# 1. Make User 
+# 2. Make input field 
+# 3. Ask user to input a string: Please enter a string: cats
+        #  store user input
+# 4. Print user input:
+# 5. Output position of character / Output letter inputted
+
+# username = "Maria"
+# user_input = input(f"Hi {username}, Please enter a string: ")
+
+# i = 0
+# while i < len(user_input):
+#     print(f"{i} {user_input[i]}")
+#     i += 1
+
+# Q3) Ask the user for a number and use this to generate a pyramid of that height.
+# Ask user for number:
+    # need - user / input field / type int - asking for number
+# Print pyramid:
+    # Should say - Pyramid size:
+    # (show Pyramind with character) *
+    #  Should iterarte through user input
+    #  Height (output) = user input
+    # User input not predefined = while loop
+
+# pyramid = int(input("Please enter a number: "))
+
+# i = 0
+# while i < pyramid:
+#     i +=1
+#     print("*"*i)
+
+# Q4) Ask the user for a number and use this to generate 
+# a (different) pyramid of that height.
+# Ask user for number:
+    # need - user / input field / type int - asking for number
+# Print pyramid:
+    # Should say - Pyramid size:
+    # (show Pyramind with character) *
+    #  Should iterarte through user input
+    #  Height (output) = user input 
+    # Width input = empty string times user input + * times user input + empty string * user input
+    # User input not predefined = while loop
+
+#1 *************** come back to************************
+
+user_input = int(input("Please enter a number: "))
+
+i = 0
+
+while i < user_input:
+    i += 1
+
+    
+   
